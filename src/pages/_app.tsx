c95ad97da35f763ac84b18e34ce47c725946e2d3
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-10-07 17:07:45 +0900

    add: tailwind & check how we use it

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
-import "/styles/global.scss";
+import "src/styles/global.scss";
+import "src/styles/tailwind.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <>
-      <h1>Hello, Next.js!</h1>
+      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
       <img src="/img-sea.jpg" alt="" />
     </>
   );
