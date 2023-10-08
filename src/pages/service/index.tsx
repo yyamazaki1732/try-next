ed235759861a599525960610793dd764d7fcb551
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-10-08 15:33:45 +0900

    add: service page

--- src/pages/service/index.tsx
+++ src/pages/service/index.tsx
@@ -1,3 +1,13 @@
+import Block from "@/components/pages/service/block";
+const headingArray = ["法人設立", "人事", "会計", "法務"];
+
 export default function Service() {
-  return <h1>Service</h1>;
+  return (
+    <div>
+      <h1 className="text-30">Service</h1>
+      {headingArray.map((heading, index) => (
+        <Block heading={heading} />
+      ))}
+    </div>
+  );
 }
