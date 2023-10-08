1b2ac6c789a15ff2b3180e3f220dbcb52d7d88e2
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-10-08 16:25:03 +0900

    add: anchor link to service page

--- src/components/pages/service/block.jsx
+++ src/components/pages/service/block.jsx
@@ -1,6 +1,9 @@
-export default function Block({ heading }) {
+export default function Block({ id, heading }) {
   return (
-    <section className="service-block p-4 bg-gray-50 rounded-md [&:not(:first-child)]:mt-8">
+    <section
+      id={id}
+      className="service-block p-4 bg-gray-50 rounded-md [&:not(:first-child)]:mt-8"
+    >
       <h2 className="font-bold border-b-2 border-yellow-700 w-max text-yellow-700">
         {heading}
       </h2>
--- src/pages/service/index.tsx
+++ src/pages/service/index.tsx
@@ -1,13 +1,62 @@
+import { useEffect } from "react";
+import {
+  Link,
+  Button,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+} from "react-scroll";
 import Block from "@/components/pages/service/block";
-const headingArray = ["法人設立", "人事", "会計", "法務"];
-
+const blockArray = [
+  { id: "corp", heading: "法人設立" },
+  { id: "human", heading: "人事" },
+  { id: "calc", heading: "会計" },
+  { id: "genel", heading: "法務" },
+];
 export default function Service() {
+  useEffect(() => {
+    // Updating scrollSpy when the component mounts.
+    scrollSpy.update();
+
+    // Returning a cleanup function to remove the registered events when the component unmounts.
+    return () => {
+      Events.scrollEvent.remove("begin");
+      Events.scrollEvent.remove("end");
+    };
+  }, []);
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
   return (
     <div>
       <h1 className="text-30">Service</h1>
-      {headingArray.map((heading, index) => (
-        <Block heading={heading} />
+      <ul className="flex">
+        {blockArray.map((block) => (
+          <li className="border border-gray-800 [&:not(:first-child)]:border-l-0">
+            <Link
+              activeClass="active"
+              to={block.id}
+              spy={true}
+              smooth={true}
+              offset={100}
+              duration={500}
+              className="p-2 cursor-pointer"
+              key={block.id}
+            >
+              {block.heading}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      {blockArray.map((block) => (
+        <Block key={block.id} id={block.id} heading={block.heading} />
       ))}
+
+      {/* Anchors to trigger scroll actions */}
+      <a onClick={scrollToTop}>To the top!</a>
     </div>
   );
 }
