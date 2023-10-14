8746ebcc77c05aaa07d8678836097ac3324f6f61
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-10-14 21:37:07 +0900

    add const type

--- src/components/pages/useState/reloadComponent.tsx
+++ src/components/pages/useState/reloadComponent.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState, useRef } from "react";
 import { GetWindowSize } from "@/components/pages/useState/getWindowSize";
 
-export default function ReloadComponent() {
+const ReloadComponent: React.FC = () => {
   const [key, setKey] = useState(Math.random());
   const once = useRef(true);
-  const { width: currentWidth } = GetWindowSize();
-  const BREAK_POINT = 900;
-  const mediaMatches = currentWidth >= BREAK_POINT ? true : false;
+  const { width: currentWidth }: { width: number } = GetWindowSize();
+  const BREAK_POINT: number = 900;
+  const mediaMatches: boolean = currentWidth >= BREAK_POINT ? true : false;
 
   useEffect(() => {
     if (currentWidth === BREAK_POINT && once.current) {
@@ -28,4 +28,6 @@ export default function ReloadComponent() {
       />
     </div>
   );
-}
+};
+
+export default ReloadComponent;
